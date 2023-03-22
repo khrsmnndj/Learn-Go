7a6ffb8e7048eea6c03c6664cb908166595a5aa0
khrsmnndj <raresheets@gmail.com>
2023-03-22 11:26:13 +0800

    add other modules

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-import {
-	"fmt"
+import "fmt"
+import "github.com/khrsmnndj/Learn-Go/src"
+func main(){
+	fmt.Println("test")
+	start.Start()
+	otherstart.OtherStart()
 }
---
+++ src/otherstart.go
@@ -0,0 +1,7 @@
+package otherstart
+
+import "fmt"
+
+func OtherStart(){
+	fmt.Println("Say Other Hello")
+}
